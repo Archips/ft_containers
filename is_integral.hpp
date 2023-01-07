304ba494b8584b3d40a76cb702989a89260e5105
Archibald THIRION <athirion@e1r13p11.clusters.42paris.fr>
2023-01-07 18:33:49 +0100

    is_integral in progress

---
+++ is_integral.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   is_integral.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: athirion <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/07 17:38:13 by athirion          #+#    #+#             */
+/*   Updated: 2023/01/07 17:56:31 by athirion         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef IS_INTEGRAL_HPP
+# define IS_INTEGRAL_HPP
+
+namespace ft {
+
+	template <class T>
+		struct is_integral {
+
+			public :
+
+
+
+
+			private :
+
+			
+
+		};
+}
+
+#endif
