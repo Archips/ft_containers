b9dafb8284c9fe471e94300b6543ff104ee192c2
Archibald THIRION <athirion@e1r7p22.clusters.42paris.fr>
2023-01-27 16:01:20 +0100

    map: node in progress

---
+++ includes/node.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   node.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: athirion <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/27 11:48:53 by athirion          #+#    #+#             */
+/*   Updated: 2023/01/27 11:52:51 by athirion         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef NODE_HPP
+# define NODE_HPP
+
+# define RED 	0
+# define BLACK	1
+
+namespace ft {
+
+	template < class Key, class T>
+	class node {
+
+		public:
+			
+			typedef Key				key_type;
+			typedef T				mapped_value;
+
+		private:
+
+			
+
+	};
+
+}
+
+#endif
