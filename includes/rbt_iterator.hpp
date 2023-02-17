e3e0e8cb377a2e4810fe0e6fbf147f5374189d1d
Archibald Thirion <archibald@archibalds-macbook-pro.home>
2023-02-17 11:24:19 +0100

    rbt_const_iterator done

--- includes/rbt_iterator.hpp
+++ includes/rbt_iterator.hpp
@@ -108,6 +108,100 @@ namespace ft {
 				return (!(this->_current == it.base()));
 			}
 	
-};
+	};
+
+	template < class iterator >
+	class rbt_iterator: public ft::iterator<typename iterator_traits<iterator>::iterator_category,
+		typename iterator_traits<iterator>::difference_type,
+		typename iterator_traits<iterator>::value_type,
+		typename iterator_traits<iterator>::pointer,
+		typename iterator_traits<iterator>::reference>
+	{
+		protected:
+
+			iterator _current;
+
+		public:
+
+			typedef iterator													iterator_type;
+			typedef typename ft::iterator_traits<iterator>::iterator_category	iterator_category;
+			typedef typename ft::iterator_traits<iterator>::difference_type		difference_type;
+			typedef typename ft::iterator_traits<iterator>::value_type			value_type;
+			typedef typename ft::iterator_traits<iterator>::pointer				pointer;
+			typedef typename ft::iterator_traits<iterator>::reference			reference;
+
+			/*
+			 ** CONSTRUCTORS
+			 */ 
+
+			rbt_const_iterator(void): _current() {};
+
+			explicit rbt_const_iterator(const iterator_type& x): _current(x) {}
+
+			rbt_const_iterator(const rbt& other): _current(other.base()) {}
+
+			template < class U >
+			rbt_const_iterator(const rbt_iterator <U> &other): _current(other.base()) {}
+
+			~rbt_const_iterator(void) {}
+
+			rbt_const_iterator& operator=(const rbt_iterator& rhs) {
+
+				if (this != &rhs)
+					this->_current = rhs._current;
+				return (*this);
+			}
+
+			iterator_type base(void) const {
+
+				return (this->_current);
+			}
+
+			reference operator*(void) const {
+
+				return (*(this->_current));
+			}
+
+			pointer operator->(void) const {
+
+				return (&(operator*()));
+			}
+
+			rbt_const_iterator& operator++(void) {
+
+				return (this->_current->successor());
+			}
+
+			rbt_const_iterator& operator--(void) {
+
+				return (this->_current->predecessor());
+			}
+
+			rbt_const_iterator operator++(int) {
+
+				rbt_const_iterator temp = *this;
+				++(*this);
+				return (temp);
+			}
+
+			rbt_const_iterator operator--(int) {
+
+				rbt_const_iterator temp = *this;
+				--(*this);
+				return (temp);
+			}
+
+			bool operator==(const rbt_const_iterator &it) const {
+
+				return (this->_current == it.base());
+			}
+
+			bool operator!=(const rbt_const_iterator &it) const {
+
+				return (!(this->_current == it.base()));
+			}
+	
+	};
+}
 
 #endif
